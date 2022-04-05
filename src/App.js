cbd933f10af57822aa0d1ba5a926ae94a34e3ca3
Jenna Leopold <jleopold424@gmail.com>
2022-04-05 16:58:16 -0400

    add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import MoviesList from './components/MoviesList'
+import MovieDetails from './components/MovieDetails'
 
 const App = () => {
-  
   return (
     <div className="App">
-      <h3>Redux Thunk Practice</h3>
+      <h3>Movies Library</h3>
+      <Routes>
+        <Route path="/" element={<MoviesList />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+      </Routes>
     </div>
   )
 }
