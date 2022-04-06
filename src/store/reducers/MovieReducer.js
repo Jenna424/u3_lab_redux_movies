49ece0c834e3ed339d95c08ecd3095bd1e40e6ad
Jenna Leopold <jleopold424@gmail.com>
2022-04-05 20:20:12 -0400

    added MovieReducer.js

--- src/store/reducers/MovieReducer.js
+++ src/store/reducers/MovieReducer.js
@@ -1,16 +1,13 @@
-const { MOVIE_LOADING_TYPE, GET_MOVIES } = require('../types')
+const { GET_MOVIES } = require('../types')
 
 const initialState = {
-  moviesList: [],
-  moviesLoading: ''
+  movies: []
 }
 
 const MovieReducer = (state = initialState, action) => {
   switch (action.type) {
-    case MOVIE_LOADING_TYPE:
-      return { ...state, moviesLoading: action.payload }
     case GET_MOVIES:
-      return { ...state, moviesList: action.payload.results }
+      return { ...state, movies: action.payload }
     default:
       return { ...state }
   }
